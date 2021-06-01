9ff19083bfeafba9e659756fe918c12914f581c1
Diego Porras (daporr) <diego.porras@owasp.org>
2021-06-01 12:03:22 -0600

    Add basic serialization

--- src/main/java/InsecureDeserialization.java
+++ src/main/java/InsecureDeserialization.java
@@ -1,2 +1,12 @@
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
 public class InsecureDeserialization {
+
+    public static void main(String args[]) throws Exception{
+        User user = new User("Hacker", 1337);
+        FileOutputStream fos = new FileOutputStream("/tmp/user.ser");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(user);
+    }
 }
