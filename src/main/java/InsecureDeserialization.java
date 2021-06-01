37b10c3b5c7f09874924fad0455b9e80ad3ae182
Diego Porras (daporr) <diego.porras@owasp.org>
2021-06-01 14:20:09 -0600

    Add exploit class

--- src/main/java/InsecureDeserialization.java
+++ src/main/java/InsecureDeserialization.java
@@ -11,11 +11,20 @@ public class InsecureDeserialization {
         FileOutputStream fos = new FileOutputStream("/tmp/user.ser");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(user);
+        oos.flush();
+        oos.close();
+
+        //Serialize exploit
+        Exploit exploit = new Exploit("");
+        FileOutputStream fos2 = new FileOutputStream("/tmp/exploit.ser");
+        ObjectOutputStream oos2 = new ObjectOutputStream(fos2);
+        oos2.writeObject(exploit);
 
         //Deserialize the user
-        FileInputStream fileIn = new FileInputStream("/tmp/user.ser");
+        FileInputStream fileIn = new FileInputStream("/tmp/exploit.ser");
         ObjectInputStream in = new ObjectInputStream(fileIn);
         User deserializedUser = (User) in.readObject();
+
         //Confirm we got the rigth object
         System.out.println("Serialized: " + user);
         System.out.println("Deserialized: " + deserializedUser);
